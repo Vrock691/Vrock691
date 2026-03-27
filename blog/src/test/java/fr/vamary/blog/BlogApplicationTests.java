b7417d854b0347739afcf3ec459b132f4ece79dc
Valentin Mary <valentin.mary@etu.univ-amu.fr>
2026-03-27 16:11:39 +0100

    Init blog spring project

---
+++ blog/src/test/java/fr/vamary/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.vamary.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
