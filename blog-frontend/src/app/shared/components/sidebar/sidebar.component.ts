e8466cc4eb8fdae2d1d846e7e44231c480865b2d
Valentin Mary <valentin.mary@etu.univ-amu.fr>
2026-03-29 12:19:18 +0200

    feat: Responsive

--- blog-frontend/src/app/shared/components/sidebar/sidebar.component.ts
+++ blog-frontend/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 export interface SidebarLink {
@@ -13,12 +13,15 @@ export interface SidebarLink {
   standalone: true,
   imports: [RouterModule],
   templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css'],
   host: {
     class: 'block'
   },
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SidebarComponent {
+  readonly isMenuOpen = signal(false);
+
   readonly links = input<ReadonlyArray<SidebarLink>>([
     { label: 'Accueil', route: '/home', iconSrc: '/home.svg', iconAlt: 'Icone accueil' },
     { label: "Blog", route: '/blog', iconSrc: '/journal.svg', iconAlt: 'Icone blog' },
@@ -26,4 +29,12 @@ export class SidebarComponent {
     { label: 'Mes competences', route: '/skills', iconSrc: '/skills.svg', iconAlt: 'Icone competences' },
     { label: 'Mon parcours', route: '/journey', iconSrc: '/book.svg', iconAlt: 'Icone parcours' },
   ]);
+
+  toggleMenu(): void {
+    this.isMenuOpen.update((isOpen) => !isOpen);
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen.set(false);
+  }
 }
